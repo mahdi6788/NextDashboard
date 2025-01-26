95d71d8e14f54a948b0241a434044f61cc89fe0a
Mahdi Jalalifar <Mjalalimech@gmail.com>
2025-01-26 12:49:45 +0400

    c1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import "@/app/ui/global.css"
+import { inter } from "./ui/fonts";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/* here the Inter font will be applied globally and other fonts that are imported and applied in the other pages can be used as needed */}
+      {/* the Tailwind antialiased class which smooths out the font, which is not necessary. */}
+      <body className={`antialiased ${inter.className}`}>{children}</body>
     </html>
   );
 }
